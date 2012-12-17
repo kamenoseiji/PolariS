d093e63fe0169e4e28923db928b290d473c1b288
Seiji Kameno <kameno@sci.kagoshima-u.ac.jp>
2012-12-17 22:01:11 +0900

    DevLog 2012/12/17, compleated F-part and partially created X-part. 	modified:   cuda_fft_xspec.cu 	deleted:    cuda_fft_segment.cu

--- cuda_fft_xspec.cu
+++ cuda_fft_xspec.cu
@@ -9,7 +9,7 @@
 #include <cutil.h>
 #include <cufft.h>
 #define	NFFT		262144
-#define	NSEG		256
+#define	NSEG		64
 
 __device__ float2 complexMult(float2 a, float2 b)		// output a x b
 {
@@ -23,7 +23,7 @@ __device__ float2 complexMultConj(float2 a, float2 b)		// output a x b*
 
 __device__ float complexMod( float2 a )				// output |a|^2
 {
-	return  a.x* a.x + a.y* a.y 
+	return  a.x* a.x + a.y* a.y;
 }
 
 __global__ void complexMultConjVec(		// calculate a x b*
@@ -45,8 +45,19 @@ __global__ void complexPowerVec(		// calculate a x a*
 {
 	int tid = blockIdx.x* blockDim.x + threadIdx.x;
 	if((tid >= 0) && (tid < length)){
-		vec_out[tid] = complexMod(vec_in_a[tid]);
+		vec_out[tid] = complexMod(vec_in[tid]);
 	}
+}
+
+__global__ void accumVector(	// a <- a + b
+	float	*vec_in_a,		// Accumuration Results
+	float	*vec_in_b,		// to be accumulated
+	int		length)
+{
+    int tid = blockIdx.x* blockDim.x + threadIdx.x;
+    if((tid >= 0) && (tid < length)){
+        vec_in_a[tid] += vec_in_b[tid];
+    }
 }
 
 
@@ -55,23 +66,27 @@ main(
 	char	**argv )		// Pointer to Arguments
 {
 	int		shrd_param_id;				// Shared Memory ID
+	int		seg_index;					// Index for Segment
 	struct	SHM_PARAM	*param_ptr;		// Pointer to the Shared Param
 	struct	sembuf		sops;			// Semaphore for data access
 	float	*segdata_ptr;				// Pointer to sampled data
+	float	*xspec_ptr;					// Pointer to 1-sec-integrated Power Spectrum
 
 	cufftHandle		cufft_plan;
 	cufftComplex	*cuSpec_data;
 	cufftReal		*cuReal_data;
+	float			*cuPowerSpec;
 /*
 -------------------------------------------- Prepare for CuFFT
 */
-	cudaMalloc( (void **)&cuReal_data, SEGDATA_SIZE/2 );
-	cudaMalloc( (void **)&cuSpec_data, SEGDATA_SIZE/2 );
+	cudaMalloc( (void **)&cuReal_data, MAX_IF* MAX_seg_sec/2* MAX_seg_len * sizeof(cufftReal) );
+	cudaMalloc( (void **)&cuSpec_data, MAX_IF* MAX_seg_sec/2* (MAX_seg_len/2+1)* sizeof(cufftComplex) );
+	cudaMalloc( (void **)&cuPowerSpec, MAX_IF* (MAX_seg_len/2+1)* sizeof(float));
 	if(cudaGetLastError() != cudaSuccess){
 		fprintf(stderr, "Cuda Error : Failed to allocate memory.\n"); return(-1);
 	}
 
-	if(cufftPlan1d(&cufft_plan, NFFT, CUFFT_R2C, NSEG) != CUFFT_SUCCESS){
+	if(cufftPlan1d(&cufft_plan, NFFT, CUFFT_R2C, NSEG* MAX_IF / 2) != CUFFT_SUCCESS){
 		fprintf(stderr, "Cuda Error : Failed to create plan.\n"); return(-1);
 	}
 	
@@ -81,6 +96,7 @@ main(
 	shrd_param_id = shmget( SHM_PARAM_KEY, sizeof(struct SHM_PARAM), 0444);
 	param_ptr = (struct SHM_PARAM *)shmat(shrd_param_id, NULL, 0);
 	segdata_ptr = (float *)shmat(param_ptr->shrd_seg_id, NULL, SHM_RDONLY);
+	xspec_ptr   = (float *)shmat(param_ptr->shrd_xspec_id, NULL, 0);
 /*
 -------------------------------------------- K5 Header and Data
 */
@@ -88,25 +104,37 @@ main(
 	while(param_ptr->validity & ACTIVE){
 		if( param_ptr->validity & (FINISH + ABSFIN) ){  break; }
 
-		printf("FFT status EW\r");
+		cudaMemset( cuPowerSpec, 0, MAX_IF* (MAX_seg_len/2+1)* sizeof(float));	// Clear Power Spec
+
+		printf("FFT status EW\n");
 		sops.sem_num = (ushort)8; sops.sem_op = (short)-4; sops.sem_flg = (short)0;
         semop( param_ptr->sem_data_id, &sops, 1);
 
-		printf("FFT status ES\r");
-		cudaMemcpy( cuReal_data, segdata_ptr, SEGDATA_SIZE/2, cudaMemcpyHostToDevice);
-		printf("FFT status EF\r");
+		printf("FFT status ES\n");
+		cudaMemcpy( cuReal_data, segdata_ptr, MAX_IF* MAX_seg_sec/2* MAX_seg_len * sizeof(cufftReal), cudaMemcpyHostToDevice);
 		cufftExecR2C(cufft_plan, cuReal_data, cuSpec_data);
+		complexPowerVec<<<NFFT/2+1,  NSEG* MAX_IF/2>>>((float2 *)cuSpec_data, cuReal_data, (NFFT/2 + 1)* NSEG* MAX_IF/2);
+		printf("FFT status EF\n");
+		//---- Accumulate power spectra
+		for(seg_index=0; seg_index<MAX_seg_sec/2; seg_index++){
+			accumVector<<<NFFT/2+1, 1>>>( cuPowerSpec, &cuReal_data[seg_index* (NFFT/2+1)], NFFT/2+1); 
+		}
 
-		printf("FFT status OW\r");
+		printf("FFT status OW\n");
 		segdata_ptr += MAX_IF* MAX_seg_len* MAX_seg_sec / 2;
 		sops.sem_num = (ushort)9; sops.sem_op = (short)-4; sops.sem_flg = (short)0;
         semop( param_ptr->sem_data_id, &sops, 1);
-		printf("FFT status OS\r");
-		cudaMemcpy( cuReal_data, segdata_ptr, SEGDATA_SIZE/2, cudaMemcpyHostToDevice);
+		printf("FFT status OS\n");
+		cudaMemcpy( cuReal_data, segdata_ptr, MAX_IF* MAX_seg_sec/2* MAX_seg_len * sizeof(cufftReal), cudaMemcpyHostToDevice);
 		segdata_ptr -= MAX_IF* MAX_seg_len* MAX_seg_sec / 2;
-		printf("FFT status OF\r");
-
 		cufftExecR2C(cufft_plan, cuReal_data, cuSpec_data);
+		complexPowerVec<<<NFFT/2+1,  NSEG* MAX_IF/2>>>((float2 *)cuSpec_data, cuReal_data, (NFFT/2 + 1)* NSEG* MAX_IF/2);
+		printf("FFT status OF\n");
+		for(seg_index=0; seg_index<MAX_seg_sec/2; seg_index++){
+			accumVector<<<NFFT/2+1, 1>>>( cuPowerSpec, &cuReal_data[seg_index* (NFFT/2+1)], NFFT/2+1); 
+		}
+
+		cudaMemcpy( xspec_ptr, segdata_ptr, (NFFT/2+1)* sizeof(float), cudaMemcpyDeviceToHost);
 	}
 /*
 -------------------------------------------- RELEASE the SHM
@@ -114,5 +142,6 @@ main(
 	cufftDestroy(cufft_plan);
 	cudaFree(&cuReal_data);
 	cudaFree(&cuSpec_data);
+	cudaFree(&cuPowerSpec);
     return(0);
 }
