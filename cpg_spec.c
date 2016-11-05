668641a62fe6b96b552418ddb6ec09c8c868bd7f
polaris <polaris@PolariN.(none)>
2016-11-06 00:21:48 +0900

    modified:   cpg_spec.c 	modified:   shm_k5data.inc 	modified:   shm_spec_view.c

--- cpg_spec.c
+++ cpg_spec.c
@@ -10,24 +10,22 @@
 int	cpg_spec(
 	struct SHM_PARAM	*param_ptr,
 	float	*freq_ptr,			// Pointer to Frequency
-	float	*xspec_ptr)			// Pointer to Spectral Data
+	float	*spec_ptr)			// Pointer to Spectral Data
 {
 	float	xmin, xmax;				// Plot Window Range
 	float	ymin, ymax;				// Plot Window Range
-	float	plot_y[NFFT2];			// Y values to plot
-	int		index;					// channel index
+	float	plot_y[MAX_CH_VIEW];	// Y values to plot
+	int		ch_index;					// channel index
 	int		st_index;				// Index for Sub-Stream
 	int		nxwin, nywin;			// Number of Panels in X and Y
 	int		nx_index, ny_index;		// Index for Panels
 	int		err_code;				// Error Code
-	double	freq_incr;				// Frequency Increment
 	float	xwin_incr,	ywin_incr;	// Position Increment of Panels
 	float	x_text, y_text;			// Text Drawing Position
 	float	peakVal;				// Line peak value to display [dB]
 	char	text[256];				// Text to Write
 
 	cpgsch(0.5);
-	freq_incr = (double)(param_ptr->fsample) / 2 / NFFT2;
 	nxwin   = (int)sqrt((double)param_ptr->num_st);
 	nywin   = (param_ptr->num_st + nxwin - 1)/nxwin;
 	xwin_incr = 0.9 / (float)nxwin;
@@ -38,15 +36,15 @@ int	cpg_spec(
 		ny_index	= st_index / nxwin;
 
 		//-------- PLOT WINDOW --------
-		xmin = - 0.5*freq_incr;	xmax = xmin + ((double)NFFT2 - 0.5) * freq_incr;
+		xmin = freq_ptr[0];		xmax = freq_ptr[MAX_CH_VIEW -1];
 		ymin = -25.0;			ymax = -5.0;
 		peakVal = -1.0e6;		// Reset Peak Value
-		for(index=0; index<NFFT2; index++){
-			plot_y[index] = 10.0* log10(xspec_ptr[st_index* NFFT2 + index]);	// autocorr. in dB
+		for(ch_index=0; ch_index<MAX_CH_VIEW; ch_index++){
+			plot_y[ch_index] = 10.0* log10(spec_ptr[st_index* MAX_CH_VIEW + ch_index]);	// autocorr. in dB
 		}
 		//-------- Peak Search
-		for(index=0.1*NFFT2; index<0.9*NFFT2; index++){
-			peakVal = MAX( peakVal, plot_y[index] );
+		for(ch_index=0.1*MAX_CH_VIEW; ch_index<0.9*MAX_CH_VIEW; ch_index++){
+			peakVal = MAX( peakVal, plot_y[ch_index] );
 		}
 		cpgsvp(	0.067+xwin_incr*nx_index, 0.067+xwin_incr*(nx_index+0.9),
 				0.067+ywin_incr*ny_index, 0.067+ywin_incr*(ny_index+0.9));
@@ -55,7 +53,7 @@ int	cpg_spec(
 		cpgsci(2);	cpgrect(xmin, xmax, ymin, ymax);
 		cpgsci(0);	cpgbox("G", 0.0, 0, "G", 10.0, 0);
 		cpgsci(1);	cpgbox(	"BCNTS", 0.0, 0, "BCNTS", 10.0, 10);
-		cpgsci(3);	cpgline( NFFT2, freq_ptr, plot_y );
+		cpgsci(3);	cpgline( MAX_CH_VIEW, freq_ptr, plot_y );
 
 		//-------- IF number
 		x_text = xmin*0.3 + xmax*0.7; y_text = ymin*0.1 + ymax*0.9;
--- shm_spec_view.c
+++ shm_spec_view.c
@@ -16,8 +16,10 @@ main(
 	int		shrd_param_id;				// Shared Memory ID
 	struct	SHM_PARAM	*param_ptr;		// Pointer to the Shared Param
 	struct	sembuf		sops;			// Semaphore for data area
-	int		index;
+	int		ch_index;					// Index for channel
+	int		bunchNum;					// Number of bunching channels	
 	float	*xspec_ptr;					// Pointer to Cross Spectra
+	float	*spec_ptr;					// Pointer to spectrum to plot
 	float	*freq_ptr;					// Pointer to Frequency
 	float	freq_incr;					// Freqnecy increment
 	char	pg_text[256];				// Text to plot
@@ -36,13 +38,14 @@ main(
 	sleep(1);									// Timing adjustment for PGPLOT device
 	setvbuf(stdout, (char *)NULL, _IONBF, 0);	// Disable stdout cache
 	cpgbeg(1, argv[1], 1, 1);
-	sprintf(xlabel, "Frequency [Hz]\0"); cpg_setup(xlabel);
-	freq_ptr = (float *)malloc(NFFT2* sizeof(float));
-	freq_incr = (double)(param_ptr->fsample) / 2 / NFFT2;
-	for(index=0; index<NFFT2; index ++){
-		freq_ptr[index] = (float)((index - 0.5)* freq_incr);
+	sprintf(xlabel, "Frequency [MHz]"); cpg_setup(xlabel);
+	freq_ptr = (float *)malloc(MAX_CH_VIEW* sizeof(float));
+	spec_ptr = (float *)malloc(NST* MAX_CH_VIEW* sizeof(float));
+	freq_incr = (double)(param_ptr->fsample) * 5.0e-7 / MAX_CH_VIEW;
+	for(ch_index=0; ch_index<MAX_CH_VIEW; ch_index ++){
+		freq_ptr[ch_index] = (float)((ch_index - 0.5)* freq_incr);
 	}
-
+	bunchNum = NFFT2 / MAX_CH_VIEW;
 	while(param_ptr->validity & ACTIVE){
 		if( param_ptr->validity & (FINISH + ABSFIN) ){  break; }
 
@@ -51,10 +54,34 @@ main(
 		semop( param_ptr->sem_data_id, &sops, 1);
 
 		//-------- Plot spectra using PGPLOT
-		cpg_spec(param_ptr, freq_ptr, xspec_ptr);
+		bunchVec(NST*NFFT2, bunchNum, xspec_ptr, spec_ptr);
+		cpg_spec(param_ptr, freq_ptr, spec_ptr);
 	}
 	cpgend();
 //------------------------------------------ RELEASE the SHM
 	free(freq_ptr);
+	free(spec_ptr);
     return(0);
+}
+
+int bunchVec(
+	int		vecLen,		// Length of the input vector
+	int		bunchNum,	// Number of channels to bunch
+	float	*vec_ptr,	// Pointer to the input vector
+	float	*out_ptr)	// Pointer to the output vector
+{
+	int	outNum;
+	int	index=0;
+	int	out_index, avg_index;
+
+	outNum = vecLen / bunchNum;
+	memset(out_ptr, 0x00, outNum* sizeof(float));
+	for(out_index=0; out_index<outNum; out_index++){
+		for(avg_index=0; avg_index<bunchNum; avg_index++){
+			out_ptr[out_index] += vec_ptr[index];
+			index++;
+		}
+		out_ptr[out_index] /= bunchNum;
+	}
+	return(outNum);
 }
